b3af018886b8b635f9c009105ea2892c07fa745a
jiomarlison <69907470+jiomarlison@users.noreply.github.com>
2023-05-27 15:39:57 -0300

    Add files via upload

--- Desafio 0/exercicio 2.py
+++ Desafio 0/exercicio 2.py
@@ -1,18 +1,18 @@
 lista = [3, 8, 50, 5, 1, 18, 12]
 lista = sorted(lista, reverse=True)
-lista_resultados = []
+lista_parciais = []
 resultado_final = []
 menor = 999_999_999_999
 
-print(lista)
+print('Lista de elementos: ', lista)
 for x in range(len(lista)):
     for y in range(1, len(lista)):
         if x != y and lista[x] - lista[y] > 0:
-            lista_resultados.append([lista[x], lista[y], lista[x] - lista[y]])
+            lista_parciais.append([lista[x], lista[y], lista[x] - lista[y]])
             if lista[x] - lista[y] < menor:
                 menor = lista[x] - lista[y]
 
-for x in lista_resultados:
-    if x[2] == menor:
-        resultado_final.append(tuple([x[0], x[1]]))
-print(resultado_final)
+for itens in lista_parciais:
+    if itens[2] == menor:
+        resultado_final.append(tuple([itens[0], itens[1]]))
+print("Números com a menor diferença absoluta: ", resultado_final)
--- Desafio 0/exercicio 3.py
+++ Desafio 0/exercicio 3.py
@@ -1,16 +1,10 @@
 from itertools import combinations
 
-lista = [1, 2, 3, 4]
+lista = [1, 2, 3]
 subconjuntos = []
 
 for x in range(0, len(lista) + 1):
-    if x == 0:
-        subconjuntos.append([])
-    if x == 1:
-        for item in lista:
-            subconjuntos.append([item])
-    if x > 1:
-        tupla_combinacoes = list(map(list, list(combinations(lista, x))))
-        subconjuntos.extend(tupla_combinacoes)
+    tupla_combinacoes = list(map(list, list(combinations(lista, x))))
+    subconjuntos.extend(tupla_combinacoes)
 
 print(subconjuntos)
