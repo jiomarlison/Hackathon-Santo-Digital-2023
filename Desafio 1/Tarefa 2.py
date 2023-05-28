8bcc73ac2905d40019352abaca74c5b675990bbf
jiomarlison <69907470+jiomarlison@users.noreply.github.com>
2023-05-28 13:29:24 -0300

    Add files via upload

--- Desafio 1/Tarefa 2.py
+++ Desafio 1/Tarefa 2.py
@@ -1,26 +1,47 @@
 import pandas as pd
-def bikes():
-    categorias = pd.read_csv("AdventureWorks_Product_Categories.csv")
-    sub_categorias = pd.read_csv("AdventureWorks_Product_Subcategories.csv")
-    produtos = pd.read_csv("AdventureWorks_Products.csv")
+categorias = pd.read_csv("AdventureWorks_Product_Categories.csv")
+sub_categorias = pd.read_csv("AdventureWorks_Product_Subcategories.csv")
+produtos = pd.read_csv("AdventureWorks_Products.csv")
+vendas2015 = pd.read_csv("AdventureWorks_Sales_2015.csv")
+vendas2016 = pd.read_csv("AdventureWorks_Sales_2016.csv")
+vendas2017 = pd.read_csv("AdventureWorks_Sales_2017.csv")
+vendastotais = pd.concat([vendas2015, vendas2016, vendas2017])
+# clientes = pd.read_excel('AdventureWorks_Customers.xlsx')
 
-    bikes = categorias.loc[categorias['CategoryName']=='Bikes']
-    print('Subcategoias Bicicletas\n', sub_categorias.loc[sub_categorias['ProductCategoryKey']==1])
-    df_cat_subcat = pd.merge(bikes, sub_categorias, how='inner')
-    print(df_cat_subcat)
-    df_cat_subcat_prod = pd.merge(df_cat_subcat, produtos, how='inner')
-    print(df_cat_subcat_prod[['CategoryName', 'ProductCategoryKey', 'ProductSubcategoryKey','ProductSKU','ProductName','ModelName']].head(10))
-    # df_cat_subcat_prod.to_csv("bicicletas", sep=',', index=False)
-    # df_cat_subcat_prod.to_excel("bicicletas.xlsx", index=False)
+#melhores vendas por produto
+vendas_totais_por_produto = vendastotais.groupby(['ProductKey'], as_index=False).sum().sort_values(by='OrderQuantity', ascending=False)
 
-def vendas():
-    vendas2015 = pd.read_csv("AdventureWorks_Sales_2015.csv")
-    # print(vendas2015.head(10))
-    vendas2016 = pd.read_csv("AdventureWorks_Sales_2016.csv")
-    # print(vendas2016.head(10))
-    vendas2017 = pd.read_csv("AdventureWorks_Sales_2017.csv")
-    # print(vendas2017.head(10))
-    vendas151617 = pd.concat([vendas2015,vendas2016,vendas2017], keys=[2015,2016,2017])
-    print(vendas151617)
+#10 BICICLETAS MAIS VENDIDAS
+categoria_bikes = categorias.loc[categorias['CategoryName'] == 'Bikes']
+subcategoria_bikes = pd.merge(categoria_bikes, sub_categorias, how='inner')
+produtos_bikes = pd.merge(subcategoria_bikes, produtos, how='inner')
+top_bikes = pd.merge(vendas_totais_por_produto, produtos_bikes, how='inner')
+top_bikes = top_bikes.rename(columns={'OrderQuantity': 'Quantidade'}).head(10)
+top_bikes = top_bikes.loc[:, ['ProductKey', 'ProductName', 'Quantidade']]
+top_bikes = top_bikes.set_index('ProductKey')
+top_bikes.to_csv('10_Bicicletas_Mais_Vendidas.csv')
 
-vendas()
+# CLIENTE COM MAIS PEDIDOS
+pedidos_totais_clientes = vendastotais.groupby(['CustomerKey'], as_index=True).count()
+pedidos_totais_clientes = pedidos_totais_clientes.sort_values(by='OrderQuantity', ascending=False).head(1)
+pedidos_totais_clientes = pedidos_totais_clientes.loc[:, ['OrderQuantity']]
+pedidos_totais_clientes.to_csv('Cliente_com_mais_pedidos.csv')
+
+# TOTAL VENDA POR MÊS
+total_vendas_meses = produtos
+total_vendas_meses['Lucro'] = total_vendas_meses['ProductPrice'] - total_vendas_meses['ProductCost']
+total_vendas_meses = pd.merge(total_vendas_meses, vendastotais, how='inner')
+total_vendas_meses['OrderDate'] = pd.to_datetime(total_vendas_meses['OrderDate'])
+total_vendas_meses['Mes'] = total_vendas_meses['OrderDate'].dt.month
+total_vendas_meses['Total'] = total_vendas_meses['OrderQuantity'] * total_vendas_meses['Lucro']
+total_vendas_territorio = total_vendas_meses
+total_vendas_meses = pd.DataFrame(total_vendas_meses.loc[:, ['Mes', 'Total']]).sort_values(by=['Mes'])
+total_vendas_meses = total_vendas_meses.groupby(['Mes']).sum()
+total_vendas_meses.to_csv('Total_Vendas_Meses.csv')
+
+#VENDAS POR REGIÃO
+total_vendas_territorio.to_excel('Venda por territorio.xlsx')
+# total_vendas_territorio = pd.DataFrame(total_vendas_territorio.loc[:, ['TerritoryKey','Total']]).sort_values(by=['TerritoryKey'])
+# print(total_vendas_territorio.groupby(['TerritoryKey']).sum())
+# media_territorios = total_vendas_territorio['Total'].mean()
+# print(media_territorios)
